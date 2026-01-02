1cc61d9b71948b84950073697b089f3957197c3f
Jeevan W Prakash <147902858+notjwp@users.noreply.github.com>
2026-01-02 23:57:36 +0530

    Delete tk_test.py

--- tk_test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title('Tkinter Test Window')
-root.geometry('400x200')
-label = tk.Label(root, text='If you see this, Tkinter works!', font=('Arial', 16))
-label.pack(pady=40)
-root.mainloop()
